246466cb4f1fce5e206a1bae52b4bfff1ccf1b66
Phuker <Phuker@users.noreply.github.com>
2021-03-22 19:48:33 +0800

    Version 0.4.19: Revert @name

--- BanBadWebsites.user.js
+++ BanBadWebsites.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Ban Bad Websites 标记/屏蔽采集站内容农场机器翻译 SEO 垃圾网站
+// @name         Ban Bad Websites 标记/屏蔽机器翻译 SEO 垃圾网站
 // @namespace    https://greasyfork.org/zh-CN/scripts/389721-ban-bad-websites
-// @version      0.4.18
+// @version      0.4.19
 // @description  标记/禁用垃圾网站链接。主要针对机器翻译 Stack Overflow，爬虫采集站内容农场等 SEO 垃圾网站。
 // @author       Phuker
 // @match        *://*/*
