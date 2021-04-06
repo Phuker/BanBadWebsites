51cfdce487ed095221723fdf1f3f99fd18fc7b00
Phuker <Phuker@users.noreply.github.com>
2021-04-07 06:55:56 +0800

    Version 0.4.23

--- BanBadWebsites.user.js
+++ BanBadWebsites.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ban Bad Websites 标记/屏蔽机器翻译 SEO 垃圾网站
 // @namespace    https://greasyfork.org/zh-CN/scripts/389721-ban-bad-websites
-// @version      0.4.22
+// @version      0.4.23
 // @description  标记/禁用垃圾网站链接。主要针对机器翻译 Stack Overflow，爬虫采集站内容农场等 SEO 垃圾网站。
 // @author       Phuker
 // @match        *://*/*
@@ -256,10 +256,12 @@ GNU General Public License v3.0
             'lsd365.com',
             'maiyewang.com',
             'secn.net',
+            'yehe.org',
             'qishunwang.net',
             'w3xue.com',
             'zl.rs',
             'joyk.com',
+            'pythonf.cn',
         ],
         '博彩垃圾网站': [
             'ceptchina.com',
